6f1aab77abfa515c2f0234af85bfd684e487ba0b
cmill <ajackal244@gmail.com>
2018-08-17 08:06:46 -0400

    updated documentation.

--- cherrywasp/accesspoint.py
+++ cherrywasp/accesspoint.py
@@ -3,16 +3,13 @@ from logger import CherryLogger
 
 
 class CherryAccessPoint:
-    """ Cherry Access Point:
+    """ An object that represents an Access Point seen in the environment.
 
-        An object that represents an Access Point seen in the environment.
+    Inputs: bssid(str) the MAC address of the device sending beacon frames.
 
-        1. Type defines it as an access point.
-        2. BSSID is the MAC address of the access point.
-        3. beaconed_essid is a list that holds all of the essids the MAC address has beaconed
-        4. evil_access_point is False by default, but switches to True if it appear to beaconing too many ESSIDs
+    Returns: prints to console and .csv file.
 
-        add_new_essid adds a new ESSID or network name to the list for the particular client.
+    add_new_essid adds a new ESSID or network name to the list for the particular client.
     """
     def __init__(self, bssid):
         self.type = "access_point"
--- cherrywasp/client.py
+++ cherrywasp/client.py
@@ -3,14 +3,11 @@ from logger import CherryLogger
 
 
 class CherryClient:
-    """ Cherry Client
-        An object that represents a wireless client seen in that environment.
+    """An object that represents a wireless client seen in that environment.
 
-        1. Type defines it as a client.
-        2. BSSID is the MAC address of the client seen.
-        3. requested_essid is all of the ESSIDs that the client has requested.
+    Inputs: bssid(str) the MAC address of the wireless client.
 
-        add_new_essid adds a new ESSID or network name to the list for the particular client.
+    Returns: prints results to console and .csv file.
     """
     def __init__(self, bssid):
         self.type = "client"
