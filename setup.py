cdb41228de9b09fb37e89800ac3daf4d9c4b5b4a
cmill <ajackal244@gmail.com>
2018-08-25 23:20:08 -0400

    create setup.py file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cherrywasp',
+    version='',
+    packages=[''],
+    package_dir={'': 'cherrywasp'},
+    url='',
+    license='',
+    author='icebath',
+    author_email='ajackal244@gmail.com',
+    description=''
+)
