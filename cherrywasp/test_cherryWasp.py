61f8ad3f72a7437e2020476c249c3b0536c85420
chris <ajackal244@gmail.com>
2018-09-05 11:39:21 -0400

    added test_scan_packet_probe_req

--- cherrywasp/test_cherryWasp.py
+++ cherrywasp/test_cherryWasp.py
@@ -11,13 +11,22 @@ class TestCherryWasp(TestCase):
     # def test_channel_hop(self):
     #     self.fail()
 
-    def test_scan_packet(self):
+    def test_scan_packet_beacon(self):
         scan_type = "0"
         expected = 'My_SSID'
         pkt_ap = Dot11(addr1="de:ad:be:ef:01:23", addr2="00:00:11:11:22:22") / Dot11Beacon() / Dot11Elt(ID=0, info="My_SSID")
         cherry_wasp = CherryWasp(scan_type)
         actual = cherry_wasp.scan_packet(pkt_ap)
-        self.assertIn(expected, actual, msg="SSIDs match.")
+        self.assertIn(expected, actual, msg="Failed SSID match.")
+        # self.fail()
+
+    def test_scan_packet_probe_req(self):
+        scan_type = "1"
+        expected = 'My_SSID'
+        pkt_pr = Dot11(addr1="de:ad:be:ef:01:23", addr2="00:00:11:11:22:22") / Dot11ProbeReq() / Dot11Elt(ID=0, info="My_SSID")
+        cherry_wasp = CherryWasp(scan_type)
+        actual = cherry_wasp.scan_packet(pkt_pr)
+        self.assertIn(expected, actual, msg="Failed SSID match.")
         # self.fail()
 
 
