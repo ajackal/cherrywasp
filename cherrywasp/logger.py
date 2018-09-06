a47bbf32a847e8c40c53981fb805c8c7a8bde329
chris <ajackal244@gmail.com>
2018-09-06 13:04:45 -0400

    added try/except to logging file

--- cherrywasp/logger.py
+++ cherrywasp/logger.py
@@ -13,24 +13,33 @@ class CherryLogger:
         self.file_path = os.path.join(os.getcwd(), "logs")
 
     def file_setup(self, file_prefix):
-        self.file_name_prefix = file_prefix
-        if os.path.exists(self.file_path) is False:
-            os.mkdir(os.path.join(os.getcwd(), "logs"))
-        self.write_headers()
-        return True
+        try:
+            self.file_name_prefix = file_prefix
+            if os.path.exists(self.file_path) is False:
+                os.mkdir(os.path.join(os.getcwd(), "logs"))
+            self.write_headers()
+            return True
+        except OSError:
+            return False
 
     def write_headers(self):
-        packet_types = ["beacon", "probe_request"]
-        for packet_type in packet_types:
-            file_name = self.file_name_prefix + "_" + packet_type + ".csv"
-            full_path = os.path.join(self.file_path, file_name)
-            with open(full_path, "w") as f:
-                f.write(self.headers)
-        return True
+        try:
+            packet_types = ["beacon", "probe_request"]
+            for packet_type in packet_types:
+                file_name = self.file_name_prefix + "_" + packet_type + ".csv"
+                full_path = os.path.join(self.file_path, file_name)
+                with open(full_path, "w") as f:
+                    f.write(self.headers)
+            return True
+        except OSError:
+            return False
 
     def write_to_file(self, packet_type, bssid, essid):
-        file_name = self.file_name_prefix + "_" + packet_type + ".csv"
-        full_path = os.path.join(self.file_path, file_name)
-        with open(full_path, "a") as r:
-            r.write("{0},{1}\n".format(bssid, essid))
-            return True
+        try:
+            file_name = self.file_name_prefix + "_" + packet_type + ".csv"
+            full_path = os.path.join(self.file_path, file_name)
+            with open(full_path, "a") as r:
+                r.write("{0},{1}\n".format(bssid, essid))
+                return True
+        except OSError:
+            return False
