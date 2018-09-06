f5e51935999ab79a3f743b05520c3e5b70b9c50c
chris <ajackal244@gmail.com>
2018-09-06 13:20:09 -0400

    fixed unit test for logger class

--- cherrywasp/test_cherryLogger.py
+++ cherrywasp/test_cherryLogger.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from cherrywasp import logger
 from datetime import datetime
+import os
 
 
 class TestCherryLogger(TestCase):
@@ -8,8 +9,13 @@ class TestCherryLogger(TestCase):
     def test_file_setup(self):
         now = datetime.now()
         file_prefix = str(now.year) + str(now.month) + str(now.day)
+        filename = file_prefix + "_beacon.csv"
         log = logger.CherryLogger()
-        expected = log.file_setup(file_prefix)
+        log.file_setup(file_prefix)
+        if os.path.isdir(os.path.join(os.getcwd(), "logs")) and os.path.isfile(os.path.join(os.getcwd(), "logs", filename)):
+            expected = True
+        else:
+            expected = False
         self.assertTrue(expected, msg="Log file setup failed.")
         # self.fail()
 
@@ -23,12 +29,21 @@ class TestCherryLogger(TestCase):
         # self.fail()
 
     def test_write_to_file(self):
+        now = datetime.now()
+        file_prefix = str(now.year) + str(now.month) + str(now.day)
+        filename = file_prefix + "_beacon.csv"
+        log_file = os.path.join(os.getcwd(), "logs", filename)
         bssid = "de:ad:be:ef:01:23"
         essid = "FBI_van_1"
         now = datetime.now()
         file_prefix = str(now.year) + str(now.month) + str(now.day)
         log = logger.CherryLogger()
         log.file_name_prefix = file_prefix
-        expected = log.write_to_file("probe_request", bssid, essid)
+        log.write_to_file("beacon", bssid, essid)
+        with open(log_file) as open_file:
+            if essid in open_file.read():
+                expected = True
+            else:
+                expected = False
         self.assertTrue(expected, msg="Log file write new entry failed.")
         # self.fail()
