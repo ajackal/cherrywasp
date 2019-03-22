75b8d24994ca0b5e8f3f180a180bb00d009da378
cmill <ajackal244@gmail.com>
2019-03-22 19:55:53 -0400

    messing with travis config2.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='cherrywasp',
-    version='',
-    packages=['scapy','termcolor', 'pytest'],
-    package_dir={'': 'cherrywasp/cherrywasp'},
-    url='',
+    version='2.0',
+    packages=['cherrywasp'],
+    url='https://github.com/ajackal/cherry-wasp.git',
     license='',
     author='cmiller',
     author_email='ajackal244@gmail.com',
-    description=''
+    description='802.11 beacon and probe request scanner.',
+    install_requires=['scapy', 'termcolor', 'pytest']
 )
