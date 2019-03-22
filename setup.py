970dec5ed0574eb511577fc338789e9222468933
cmill <ajackal244@gmail.com>
2019-03-22 19:52:19 -0400

    messing with travis config.

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='cherrywasp',
     version='',
-    packages=[''],
-    package_dir={'': 'cherrywasp'},
+    packages=['scapy','termcolor', 'pytest'],
+    package_dir={'': 'cherrywasp/cherrywasp'},
     url='',
     license='',
-    author='icebath',
+    author='cmiller',
     author_email='ajackal244@gmail.com',
     description=''
 )
