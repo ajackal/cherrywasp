741b15c77df65e083459aae6824b2adc0659fa38
cmill <ajackal244@gmail.com>
2019-03-22 20:08:35 -0400

    testing new project name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cherrywasp',
-    version='2.0',
+    version='2.1',
     packages=['cherrywasp'],
     url='https://github.com/ajackal/cherry-wasp.git',
     license='',
